9c5636f614f6be40c2466d80a9464b4040992fc2
Ricky <rickytsang.dev@gmail.com>
2025-02-28 06:01:11 +0100

    import dotenv to handle environment variable

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ const PORT = parseInt(process.env.PORT) || 8800;
 
 app.use(express.json());
 
+// import dotenv 
+require("dotenv").config();
+// mongodb connetion with mongoose
+const mongoose = require("mongoose");
+const uri = process.env.DB_CONNECT;
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -13,4 +36,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-//
